75e2b15d10c07946167519865a147125792b4765
Michael Dickinson <michael.dickinson3949@gmail.com>
2026-06-01 16:31:08 -0700

    init

---
+++ temporary_install.sh
@@ -0,0 +1,18 @@
+# Source Originals
+source ~/.zshrc
+
+# Source our zshrc
+source .zshrc
+
+# Temp dotfile dir
+# DOTFILES_TMP=$(mktemp -d)
+# git -C $DOTFILES_TMP clone https://github.com/Michael-R-Dickinson/dotfiles.git .
+DOTFILES_TMP=.
+
+# Set shell and dotfiles paths
+export XDG_CONFIG_HOME="$DOTFILES_TMP"
+export ZDOTDIR="$DOTFILES_TMP"
+
+# Alias tools to use config files
+alias vim='vim -u $DOTFILES_TMP/.vimrc'
+alias tmux='tmux -f $DOTFILES_TMP/.tmux.conf'
