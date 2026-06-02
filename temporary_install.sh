c109c7329abe2813c2cac71fda9266cce7930232
Michael Dickinson <michael.dickinson3949@gmail.com>
2026-06-01 17:07:47 -0700

    fix temporary install script

--- temporary_install.sh
+++ temporary_install.sh
@@ -1,13 +1,20 @@
 # Source Originals
 source ~/.zshrc
 
-# Source our zshrc
-source .zshrc
-
 # Temp dotfile dir
-# DOTFILES_TMP=$(mktemp -d)
-# git -C $DOTFILES_TMP clone https://github.com/Michael-R-Dickinson/dotfiles.git .
-DOTFILES_TMP=.
+DOTFILES_TMP=$(mktemp -d)
+git -C $DOTFILES_TMP clone https://github.com/Michael-R-Dickinson/dotfiles.git .
+# DOTFILES_TMP=.
+
+# Rename files in the form "dot_zshrc" to ".zshrc"
+(cd $DOTFILES_TMP &&
+    for file in $(ls | grep -E "dot_*"); do
+        mv "$file" ".${file#dot_}"
+    done
+)
+
+# Source our zshrc
+source $DOTFILES_TMP/.zshrc
 
 # Set shell and dotfiles paths
 export XDG_CONFIG_HOME="$DOTFILES_TMP"
@@ -18,5 +25,5 @@ alias vim='vim -u $DOTFILES_TMP/.vimrc'
 alias tmux='tmux -f $DOTFILES_TMP/.tmux.conf'
 
 # Starship
-curl -sS https://starship.rs/install.sh | sh -s -- --bin-dir $DOTFILES_TMP --yes
+curl -sS https://starship.rs/install.sh | sh -s -- --bin-dir $DOTFILES_TMP --yes > /dev/null
 export PATH="$DOTFILES_TMP:$PATH"
